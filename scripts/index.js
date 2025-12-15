ec225aa1ed346700adca0206709a5bc555636bb2
tsuroki <tsuroki0210@gmail.com>
2025-12-15 13:22:31 +0900

    first

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const mainSwiper = new Swiper(".city-swiper", {
+
+})
+
+const seoulSwiper = new Swiper(".seoul-swiper", {
+    slidesPerView: 7.1,
+    spaceBetween: 20,
+    touchReleaseOnEdges : true,
+    nested: true,
+
+})
---
+++ scripts/weather.js
@@ -0,0 +1,4 @@
+const weatherDB = [
+  { city: "서울", temp: "-6°C" },
+  { city: "인천광역시", temp: "-3°C" },
+];
